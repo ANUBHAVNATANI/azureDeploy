2b887ad8843b0b1e687d09664c65b1ad35db98f5
ANUBHAVNATANI <anubhavnatani@gmail.com>
2019-10-20 05:33:59 +0530

    Api routes additions

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from flask import Flask, request, jsonify,render_template
 import os
 import traceback
 #My library
-#from utils import get_face_image,get_embeddings,calc_dist,get_match_score
 from azure_utils import get_face_id,face_compare
+from azure_database import get_database,post_matched_images
 
 # API definition
 app = Flask(__name__)
@@ -15,39 +15,15 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('home.html')
-"""
-@app.route("/checkImagesV1",methods=["GET"])
-def checkImages():
-    try:
-        databaseName = request.args.get('databaseName')
-        imageId = request.args.get('imageId')
-        #selecting important information from the user request
-        
-        #Accuring the database images based on the request
-        #img1
-        #img2 or vector2
-        face1 = get_face_image(img1)
-        face2 = get_face_image(img2)
-        #can be skipped by the inclusion of the vectors time saved - 1s
-        vector1 = get_embeddings(face1)
-        vector2 = get_embeddings(face2)
-        #getting the distance (distance measure can be changed)
-        distOfImages = calc_dist(vector1,vector2)
-        #getting the match score of the image
-        score = get_match_score(distOfImages)
-        #passing this as a api response to the client
-        return jsonify({"Score":score})
-    except:
-        return jsonify({'trace': traceback.format_exc()})
-"""
+
 
 @app.route("/checkImagesV2",methods=["GET","POST"])
 def checkImages():
     try:
         #Api of checking for the android cliet
         faceId = request.args.get('faceId')
-        #can be skipped by the inclusion of the vectors time saved - 1s
-        a = []
+        database = get_database('perm')
+        a,name = database[0],database[1] 
         #take face ids from the original database
         #results array
         score = []
@@ -59,10 +35,11 @@ def checkImages():
         extract_max = score.index(max(score))
         if(boolArr[extract_max]==True):
             #call database route to store the array
-            pass
-        else:
-            #else do not call the database route
-            pass
+            r = post_matched_images(a[extract_max],faceId,name[extract_max])
+            if(len(r.json())==1):
+                #error handling 
+                return jsonify({"Feedback": "Error"}) 
+
         #passing this as a api response to the client
         return jsonify({"Feedback":"ThankYou"})
     except:
@@ -75,7 +52,7 @@ def getId():
         #generating the face id
         faceId = get_face_id(dbItemId)
         #get the temp database images using the database route
-        a = []
+        a = get_database("temp")
         #results array
         score = []
         boolArr = []
